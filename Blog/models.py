136d0dcb86c277820fdc2f062005b89f65301c0d
saurabhkumarverma <saurabh88871kumar@hotmail.com>
2020-05-12 12:03:42 +0530

    blog admin updated

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# # Create A blog models here.
+# title
+# Publication Date
+# body
+# image
+#
+#
+# # Add theBlog app to setting
+# # Create a mirragtion
+# # migration
+# # Add to the Admin
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pu_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
