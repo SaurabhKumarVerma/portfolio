ce96a2e371e12df8a059662e73d2b85c4cddfd96
saurabhkumarverma <saurabh88871kumar@hotmail.com>
2020-05-20 11:25:13 +0530

    Profolio Project is completed
    Few Thing is Let

--- Blog/models.py
+++ Blog/models.py
@@ -17,3 +17,10 @@ class Blog(models.Model):
     pu_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        return self.title[:100]
